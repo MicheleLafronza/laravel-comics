dca274c9e06478e1e52543f79846abb1c5dcae6f
MicheleLafronza <michele.lafronza91@gmail.com>
2024-09-10 17:02:21 +0200

    feat: add footer mid and bottom sections

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,6 +8,45 @@ $footer_top_links = [
     ["text" => "DC POWER VISA", "image" => "/img/buy-dc-power-visa.svg"],
 ];
 
+$comicsLinks = [
+    ["text" => "Characters"],
+    ["text" => "Comics"],
+    ["text" => "Movies"],
+    ["text" => "TV"],
+    ["text" => "Games"],
+    ["text" => "Videos"],
+    ["text" => "News"],
+];
+
+$shopLinks = [
+    ["text" => "Shop DC"],
+    ["text" => "Shop DC Collectibles"],
+];
+
+$DCLinks = [
+    ["text" => "Term of use"],
+    ["text" => "Privacy Policy"],
+    ["text" => "Ad Choiches"],
+    ["text" => "Advertising"],
+    ["text" => "Jobs"],
+    ["text" => "Subrscriptions"],
+    ["text" => "Talent Workshops"],
+    ["text" => "CPSC Certificates"],
+    ["text" => "Ratings"],
+    ["text" => "Shop Help"],
+    ["text" => "Contact Us"],
+];
+
+$siteLinks = [
+    ["text" => "DC"],
+    ["text" => "MAD Magazine"],
+    ["text" => "DC Kids"],
+    ["text" => "DC Universe"],
+    ["text" => "DC PowerVisa"],
+];
+
+
+
 
 @endphp
 
@@ -29,6 +68,61 @@ $footer_top_links = [
 </div>
 
 <div class="footer-mid">
+
+    <div class="container">
+
+        <div class="links">
+
+            <div class="links-sections">
+                <span class="list-title">
+                    DC COMICS
+                </span>
+                <ul>
+                    @foreach ($comicsLinks as $item)
+                        <li>{{$item['text'] }}</li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="links-sections">
+                <span class="list-title">
+                    SHOP
+                </span>
+                <ul>
+                    @foreach ($shopLinks as $item)
+                        <li>{{$item['text'] }}</li>
+                    @endforeach
+                </ul>
+            </div>
+
+            <div class="links-sections">
+                <span class="list-title">
+                    DC
+                </span>
+                <ul>
+                    @foreach ($DCLinks as $item)
+                        <li>{{$item['text'] }}</li>
+                    @endforeach
+                </ul>
+            </div>
+            
+            <div class="links-sections">
+                <span class="list-title">
+                    SITES
+                </span>
+                <ul>
+                    @foreach ($siteLinks as $item)
+                        <li>{{$item['text'] }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+
+        <div class="bg-logo">
+            <img src="img/dc-logo-bg.png" alt="">
+        </div>
+
+    </div>
 
 </div>
 
